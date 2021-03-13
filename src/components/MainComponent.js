13231a5db8e0332f09e8053e96b836b0611742b8
Kaleigh Deardorff <kaleigh.doodlepip@gmail.com>
2021-03-12 20:08:46 -0800

    Rearranged the components to enable the use of routes and paths

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -12,20 +12,24 @@ class Main extends Component {
         super(props);
         this.state = {
             campsites: CAMPSITES,
-            selectedCampsite: null
         };
     }
 
-    onCampsiteSelect(campsiteId) {
-        this.setState({selectedCampsite: campsiteId});
-    }
-
     render() {
+        const HomePage = () => {
+            return(
+                <Home />
+            );
+        }
+
         return (
             <div>
                 <Header />
-                <Directory campsites={this.state.campsites} onClick={campsiteId => this.onCampsiteSelect(campsiteId)}/>
-                <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]} />
+                <Switch>
+                    <Route path='/home' component={HomePage} />
+                    <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+                    <Redirect to='/home' />
+                </Switch>
                 <Footer />
             </div>
         );
