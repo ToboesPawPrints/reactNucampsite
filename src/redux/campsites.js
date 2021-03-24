26ba41387ec05ab00e0863659df2cddf78ec701e
Kaleigh Deardorff <kaleigh.doodlepip@gmail.com>
2021-03-24 15:29:09 -0700

    Updated files to incorporate Redux Tunk library

--- src/redux/campsites.js
+++ src/redux/campsites.js
@@ -1,8 +1,18 @@
-import { CAMPSITES } from '../shared/campsites';
+import * as ActionTypes from './ActionTypes';
 
-export const Campsites = (state = CAMPSITES, action) => {
+export const Campsites = (state = {
+    isLoading: true,
+    errMes: null,
+    campsites: []
+    }, action) => {
     switch (action.type) {
+            case ActionTypes.ADD_CAMPSITES:
+                return {...state, isLoading: false, errMess: null, campsites: action,payload};
+            case ActionTypes.CAMPSITES_LOADING:
+                return {...state, isloading: true, errMess: null, campsites: []};
+            case ActionTypes.CAMPSITES_FAILED:
+                return {...state, isLoading: false, errMess: action.payload };
         default:
             return state;
     }
-}
+};
